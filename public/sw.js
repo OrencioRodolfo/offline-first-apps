d0fd32561e09bb6f520bd4e5d029acd3e4e2ad9b
OrencioRodolfo <orencio.goncalves@exictos.com>
2016-07-07 19:56:49 +0100

    Add favicon

--- public/sw.js
+++ public/sw.js
@@ -16,6 +16,7 @@ self.addEventListener('install', function(event) {
         'templates/banks-wrapper.html',
         'templates/banks-listing.html',
         'img/no-logo.png',
+        'img/offline.png',
       ]);
     })
   );
