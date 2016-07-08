9d0e60549c7db3ac5d567bf4e250a3f778e8e15c
OrencioRodolfo <orencio.goncalves@exictos.com>
2016-07-08 16:42:29 +0100

    Remove service worker

--- public/js/sw-register.js
+++ public/js/sw-register.js
@@ -1,11 +1,3 @@
 (function() {
-  console.log('asdasd');
-  if (!navigator.serviceWorker)
-    return;
-
-  navigator.serviceWorker.register('./../sw.js').then(function(registration) {
-    console.log('Registration worked!', registration.scope);
-  }).catch(function(e) {
-    console.log('Service Woker registration failed!', e);
-  });
+  // @TODO regist our brand new SW. Make sure that the browser supports it
 })();
