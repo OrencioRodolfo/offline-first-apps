15e9134640e295553fe3c8b56cc3e3b1285a6d79
orenciorodolfo <orenciorodolfo@hotmail.com>
2016-07-08 19:59:37 +0100

    Remove IDB

--- public/js/app/services/banks.js
+++ public/js/app/services/banks.js
@@ -1,21 +1,11 @@
-// require our IndexedDB interface for caching banks info
-var BanksCache = require('./../../idb');
-
 angular
   .module('offlineFirst')
   .factory('BanksService', ['$http', function($http) {
      var banks = [];
      var get = function() {
-       var banksCache = new BanksCache();
-
-       return banksCache.getBanks().then(function(docs){
-         var request = $http.get('/banks').then((res) => {
-           var banks = res.data.banks.slice(30, 60);
-           banksCache.setBanks(banks);
-           return banks;
-         });
-
-         return docs || request;
+       return $http.get('/banks').then((res) => {
+         var banks = res.data.banks.slice(30, 60);
+         return banks;
        });
      };
 
